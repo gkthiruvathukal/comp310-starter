d412ac7303431d0848adce635728f9bc30282b0a
Ella McKenney <ellavmckenney@gmail.com>
2024-01-25 17:10:02 +0000

    Edit WarmUp_3

--- WarmUp_3/array_operations.c
+++ WarmUp_3/array_operations.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 5
 
 int main(void) {
-    int numbers[5];
+    int index, numbers[SIZE];
     int sum = 0;
     float average;
+
+    for (index = 0; index < SIZE; index++)
+        sum += score[index];
+    average = (float) sum / SIZE;
+
+    printf
 
     return 0;
 }
