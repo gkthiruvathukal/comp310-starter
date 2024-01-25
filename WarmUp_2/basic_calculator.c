accaf8696ddab8bbe39a0e276c941a1a21228079
Ella McKenney <ellavmckenney@gmail.com>
2024-01-25 16:36:18 +0000

    Edit WarmUp_2

--- WarmUp_2/basic_calculator.c
+++ WarmUp_2/basic_calculator.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int main(int argc, char * * argv) {
+
+  if (argc < 2) {
+    printf("Need a number!\n");
+    return EXIT_FAILURE;
+  }
     double a, b;
    
       printf("Enter first number: ");
